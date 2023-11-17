267afa2244f822ab301ff00965bcd8adeb9cd8fe
Aldo Francisco Pastrana Leon <102763835+U20211C186@users.noreply.github.com>
2023-11-17 02:48:36 -0500

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
     //baseURL:"https://saveup-api-production.up.railway.app/api/saveup/v1"
 
     //baseURL: "http://localhost:8080/api/creditovehicular/v1"
-    baseURL: "https://5948-38-25-12-1.ngrok.io/api/creditovehicular/v1"
+    baseURL: "https://ce9e-38-25-12-1.ngrok.io/api/creditovehicular/v1"
 };
